faac07d7180e5ab0b9fe254d4367b43e4ead0504
NorrisAkg <93124855+NorrisAkg@users.noreply.github.com>
2023-03-14 14:17:43 +0100

    init project

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import "./bootstrap";
+
+import { createApp } from "vue";
+import home from "./app/pages/home.vue";
+
+createApp(home).mount("#app");
---
+++ resources/js/app/pages/home.vue
@@ -0,0 +1,7 @@
+<template lang="">
+    <div>
+        <h1>Hello laravel 10 !</h1>
+    </div>
+</template>
+
+<script setup></script>
